bc139570dda3ffb2e51806fe13d8ef554e941973
ljn425 <ljn425@naver.com>
2023-08-25 12:06:55 +0900

    section 01-2

---
+++ src/main/java/springsecurity/oauth2/SecurityConfig.java
@@ -0,0 +1,18 @@
+package springsecurity.oauth2;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.apply(new CustomSecurityConfigurer().setFlag(false));
+        return http.build();
+    }
+}
